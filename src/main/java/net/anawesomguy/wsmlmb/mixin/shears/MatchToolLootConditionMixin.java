8c9af11249938b331a8aaa4f4421632f83efd817
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2023-11-22 16:26:06 +0800

    downgrade to support more versions (version dependent changes only)

--- src/main/java/net/anawesomguy/wsmlmb/mixin/shears/MatchToolLootConditionMixin.java
+++ src/main/java/net/anawesomguy/wsmlmb/mixin/shears/MatchToolLootConditionMixin.java
@@ -1,7 +1,6 @@
 package net.anawesomguy.wsmlmb.mixin.shears;
 
-import com.google.common.collect.ImmutableList;
-import net.anawesomguy.wsmlmb.mixin.shears.accessors.DirectRegistryEntryListAccessor;
+import com.google.common.collect.ImmutableSet;
 import net.fabricmc.fabric.api.event.lifecycle.v1.CommonLifecycleEvents;
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalItemTags;
 import net.minecraft.item.Item;
@@ -11,6 +10,7 @@ import net.minecraft.loot.condition.MatchToolLootCondition;
 import net.minecraft.predicate.item.ItemPredicate;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.entry.RegistryEntry;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -20,38 +20,35 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Mixin(MatchToolLootCondition.class)
 public abstract class MatchToolLootConditionMixin implements LootCondition {
+    @SuppressWarnings("ShadowModifiers")
+    @Shadow @Final private ItemPredicate predicate;
     @Unique private static final List<ItemPredicate> MATCH_TOOL_PREDICATES = new ArrayList<>();
-
-    @Shadow public abstract Optional<ItemPredicate> predicate();
 
     @Inject(at = @At("RETURN"), method = "<init>")
     private void stone_utils$shearsLoot(CallbackInfo ci) {
         // allows anything in c:shears to mine grass (and other stuff) and it will drop
-        predicate().ifPresent(MATCH_TOOL_PREDICATES::add);
+        if (predicate.items != null) MATCH_TOOL_PREDICATES.add(predicate);
     }
 
     static {
         // loot is loaded before tags, so this is required
         CommonLifecycleEvents.TAGS_LOADED.register((registries, client) -> {
             if (!client) {
-                List<RegistryEntry<Item>> shears = new ArrayList<>();
+                List<Item> shears = new ArrayList<>();
                 for (RegistryEntry<Item> entry :
                         Registries.ITEM.getOrCreateEntryList(ConventionalItemTags.SHEARS))
-                    shears.add(entry);
+                    shears.add(entry.value());
 
                 for (ItemPredicate p : MATCH_TOOL_PREDICATES) {
-                    if (p.items().isPresent() && p.items().get().contains(Items.SHEARS.getRegistryEntry())) {
-                        @SuppressWarnings("unchecked")
-                        DirectRegistryEntryListAccessor<Item> accessor = ((DirectRegistryEntryListAccessor<Item>)p.items().get());
-                        ImmutableList.Builder<RegistryEntry<Item>> builder = new ImmutableList.Builder<>();
-                        builder.addAll(accessor.getEntries());
+                    //noinspection DataFlowIssue
+                    if (p.items.contains(Items.SHEARS)) {
+                        ImmutableSet.Builder<Item> builder = new ImmutableSet.Builder<>();
+                        builder.addAll(p.items);
                         builder.addAll(shears);
-                        accessor.setEntries(builder.build());
-                        accessor.setEntrySet(null);
+                        p.items = builder.build();
                     }
                 }
 
