3f7fa1f0353d2558b7b1f48d81b06228a2f4e242
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2023-11-22 16:29:35 +0800

    some bugfixes and feature additions

--- src/main/java/net/anawesomguy/wsmlmb/mixin/shears/EfficiencyEnchantmentMixin.java
+++ src/main/java/net/anawesomguy/wsmlmb/mixin/shears/EfficiencyEnchantmentMixin.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.tag.convention.v1.ConventionalItemTags;
 import net.minecraft.enchantment.EfficiencyEnchantment;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -13,6 +14,6 @@ public abstract class EfficiencyEnchantmentMixin {
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"), method = "isAcceptableItem")
     private boolean stone_utils$isShears(ItemStack stack, Item item) {
         // allows anything in c:shears to be enchanted with efficiency
-        return stack.isOf(item) || stack.isIn(ConventionalItemTags.SHEARS);
+        return stack.isOf(item) || (item == Items.SHEARS && stack.isIn(ConventionalItemTags.SHEARS));
     }
 }
