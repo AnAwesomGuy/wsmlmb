ba05449f9ae9a9f3b33063941416acd9fd3e162f
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2023-11-24 11:50:09 +0800

    some small minor changes

--- src/main/java/net/anawesomguy/wsmlmb/block/MultiVersionBlockSettings.java
+++ src/main/java/net/anawesomguy/wsmlmb/block/MultiVersionBlockSettings.java
@@ -2,7 +2,6 @@ package net.anawesomguy.wsmlmb.block;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.mixin.object.builder.AbstractBlockAccessor;
-import net.fabricmc.fabric.mixin.object.builder.AbstractBlockSettingsAccessor;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -22,12 +21,12 @@ import java.util.function.ToIntFunction;
  * A custom class for {@link AbstractBlock.Settings} for support over multiple versions. If the method doesn't exist for that version, nothing will happen.
  */
 @SuppressWarnings({"deprecation", "UnstableApiUsage"})
-public class MultiVersionBlockSettings extends FabricBlockSettings {
-    protected MultiVersionBlockSettings(String material) {
+public final class MultiVersionBlockSettings extends FabricBlockSettings {
+    private MultiVersionBlockSettings(String material) {
         super();
     }
 
-    protected MultiVersionBlockSettings(AbstractBlock.Settings settings) {
+    private MultiVersionBlockSettings(AbstractBlock.Settings settings) {
         super(settings);
     }
 
@@ -54,37 +53,31 @@ public class MultiVersionBlockSettings extends FabricBlockSettings {
         return new MultiVersionBlockSettings(settings);
     }
 
-    @Override
     public MultiVersionBlockSettings noCollision() {
         super.noCollision();
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings nonOpaque() {
         super.nonOpaque();
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings slipperiness(float value) {
         super.slipperiness(value);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings velocityMultiplier(float velocityMultiplier) {
         super.velocityMultiplier(velocityMultiplier);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings jumpVelocityMultiplier(float jumpVelocityMultiplier) {
         super.jumpVelocityMultiplier(jumpVelocityMultiplier);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings sounds(BlockSoundGroup group) {
         super.sounds(group);
         return this;
@@ -98,19 +91,16 @@ public class MultiVersionBlockSettings extends FabricBlockSettings {
         return this.luminance(levelFunction);
     }
 
-    @Override
     public MultiVersionBlockSettings luminance(ToIntFunction<BlockState> luminanceFunction) {
         super.luminance(luminanceFunction);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings strength(float hardness, float resistance) {
         super.strength(hardness, resistance);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings breakInstantly() {
         super.breakInstantly();
         return this;
@@ -121,67 +111,56 @@ public class MultiVersionBlockSettings extends FabricBlockSettings {
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings ticksRandomly() {
         super.ticksRandomly();
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings dynamicBounds() {
         super.dynamicBounds();
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings dropsNothing() {
         super.dropsNothing();
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings dropsLike(Block block) {
         super.dropsLike(block);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings air() {
         super.air();
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings allowsSpawning(AbstractBlock.TypedContextPredicate<EntityType<?>> predicate) {
         super.allowsSpawning(predicate);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings solidBlock(AbstractBlock.ContextPredicate predicate) {
         super.solidBlock(predicate);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings suffocates(AbstractBlock.ContextPredicate predicate) {
         super.suffocates(predicate);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings blockVision(AbstractBlock.ContextPredicate predicate) {
         super.blockVision(predicate);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings postProcess(AbstractBlock.ContextPredicate predicate) {
         super.postProcess(predicate);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings emissiveLighting(AbstractBlock.ContextPredicate predicate) {
         super.emissiveLighting(predicate);
         return this;
@@ -190,91 +169,76 @@ public class MultiVersionBlockSettings extends FabricBlockSettings {
     /**
      * Make the block require tool to drop and slows down mining speed if the incorrect tool is used.
      */
-    @Override
     public MultiVersionBlockSettings requiresTool() {
         super.requiresTool();
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings mapColor(MapColor color) {
         super.mapColor(color);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings hardness(float hardness) {
         super.hardness(hardness);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings resistance(float resistance) {
         super.resistance(resistance);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings offset(AbstractBlock.OffsetType offsetType) {
         super.offset(offsetType);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings noBlockBreakParticles() {
         super.noBlockBreakParticles();
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings requires(FeatureFlag... features) {
         super.requires(features);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings mapColor(Function<BlockState, MapColor> mapColorProvider) {
         super.mapColor(mapColorProvider);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings burnable() {
         super.burnable();
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings liquid() {
         super.liquid();
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings solid() {
         super.solid();
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings notSolid() {
         super.notSolid();
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings pistonBehavior(PistonBehavior pistonBehavior) {
         super.pistonBehavior(pistonBehavior);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings instrument(Instrument instrument) {
         super.instrument(instrument);
         return this;
     }
 
-    @Override
     public MultiVersionBlockSettings replaceable() {
         super.replaceable();
         return this;
@@ -297,7 +261,7 @@ public class MultiVersionBlockSettings extends FabricBlockSettings {
     }
 
     public MultiVersionBlockSettings drops(Identifier dropTableId) {
-        ((AbstractBlockSettingsAccessor)this).setLootTableId(dropTableId);
+        super.drops(dropTableId);
         return this;
     }
 
@@ -324,7 +288,7 @@ public class MultiVersionBlockSettings extends FabricBlockSettings {
     }
 
     public MultiVersionBlockSettings collidable(boolean collidable) {
-        ((AbstractBlockSettingsAccessor)this).setCollidable(collidable);
+        super.collidable(collidable);
         return this;
     }
 }
