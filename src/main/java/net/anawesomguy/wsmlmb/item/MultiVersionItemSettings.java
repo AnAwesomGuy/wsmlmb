0f23c8655ddacf82aa269d9dee5b7a829c576372
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2023-11-28 17:07:27 +0800

    move MultiVersionItemSettings

--- src/main/java/net/anawesomguy/wsmlmb/item/MultiVersionItemSettings.java
+++ src/main/java/net/anawesomguy/wsmlmb/item/MultiVersionItemSettings.java
@@ -1,8 +1,11 @@
-package net.anawesomguy.wsmlmb.block.item;
+package net.anawesomguy.wsmlmb.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.ItemGroup;
 
+/**
+ * A custom class for {@link net.minecraft.item.Item.Settings} for support over multiple versions. If the method doesn't exist for that version, nothing will happen.
+ */
 public class MultiVersionItemSettings extends FabricItemSettings {
     public MultiVersionItemSettings group(ItemGroup group) {
         super.group(group);
