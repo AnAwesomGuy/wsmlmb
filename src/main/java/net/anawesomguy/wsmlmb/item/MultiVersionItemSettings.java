330870ce68ff526b31b0c7ed71dbfee00178e83c
AnAwesomGuy <79700396+AnAwesomGuy@users.noreply.github.com>
2023-11-29 17:33:56 +0800

    nearly there

--- src/main/java/net/anawesomguy/wsmlmb/item/MultiVersionItemSettings.java
+++ src/main/java/net/anawesomguy/wsmlmb/item/MultiVersionItemSettings.java
@@ -2,17 +2,18 @@ package net.anawesomguy.wsmlmb.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.resource.featuretoggle.FeatureFlag;
 
 /**
  * A custom class for {@link net.minecraft.item.Item.Settings} for support over multiple versions. If the method doesn't exist for that version, nothing will happen.
  */
 public class MultiVersionItemSettings extends FabricItemSettings {
     public MultiVersionItemSettings group(ItemGroup group) {
-        super.group(group);
         return this;
     }
 
     public MultiVersionItemSettings requires(Object... features) {
+        super.requires((FeatureFlag[])features);
         return this;
     }
 }
--- src/test/java/net/anawesomguy/wsmlmb/test/WSMLMBTest.java
+++ src/test/java/net/anawesomguy/wsmlmb/test/WSMLMBTest.java
@@ -1,10 +1,11 @@
 package net.anawesomguy.wsmlmb.test;
 
 import net.anawesomguy.wsmlmb.block.CustomCraftingTableBlock;
+import net.anawesomguy.wsmlmb.block.MultiVersionBlockSettings;
 import net.anawesomguy.wsmlmb.block.chest.TexturedChestBlock;
+import net.anawesomguy.wsmlmb.item.MultiVersionItemSettings;
 import net.anawesomguy.wsmlmb.util.WSMLMBUtil;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -13,36 +14,35 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
-@SuppressWarnings("deprecation")
 public final class WSMLMBTest implements ModInitializer {
     public static final String MOD_ID = "wsmlmb-test";
     public static final Block STONE_CHEST = Registry.register(
         Registries.BLOCK,
         new Identifier(MOD_ID, "stone_chest"),
-        new TexturedChestBlock.Builder(FabricBlockSettings.of().strength(1.5F, 6).requiresTool())
+        new TexturedChestBlock.Builder(MultiVersionBlockSettings.of().strength(1.5F, 6).requiresTool())
             .setTextures(chestTexture("stone"), chestTexture("stone_left"), chestTexture("stone_right")) // sets the textures
             .build()
     );
     public static final Item STONE_CHEST_ITEM = Registry.register(
         Registries.ITEM,
         new Identifier(MOD_ID, "stone_chest"),
-        new BlockItem(STONE_CHEST, new Item.Settings())
+        new BlockItem(STONE_CHEST, new MultiVersionItemSettings())
     );
     public static final Block CUSTOM_CRAFTING_TABLE = Registry.register(
         Registries.BLOCK,
         new Identifier(MOD_ID, "custom_crafting_table"),
-        new CustomCraftingTableBlock(FabricBlockSettings.of())
+        new CustomCraftingTableBlock(MultiVersionBlockSettings.of())
     );
     public static final Item CUSTOM_CRAFTING_TABLE_ITEM = Registry.register(
         Registries.ITEM,
         new Identifier(MOD_ID, "custom_crafting_table"),
-        new BlockItem(CUSTOM_CRAFTING_TABLE, new Item.Settings())
+        new BlockItem(CUSTOM_CRAFTING_TABLE, new MultiVersionItemSettings())
     );
     // will be added to c:shears
     public static final Item TEST_SHEARS = Registry.register(
         Registries.ITEM,
         new Identifier(MOD_ID, "test_shears"),
-        new ShearsItem(new Item.Settings())
+        new ShearsItem(new MultiVersionItemSettings())
     );
 
     @Override
